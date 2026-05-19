31277997fa5e21aa792125f0547993816faf9c76
JoaoLuisVignoli <jlrdvignoli@gmail.com>
2026-05-19 16:40:31 -0300

    Filtro de Gabor

--- Gabor_Filter.py
+++ Gabor_Filter.py
@@ -2,10 +2,11 @@ import numpy as np
 import cv2
 
 # Implementação do filtro de Gabor por meio da fórmula disponibilzida
-def gabor_filter(size, theta, f, sigma, gamma, psi):
+def gabor_filter(x_size, y_size, theta, f, sigma, gamma, psi):
+
     # Tamanho do corpo do filtro
-    x_range = np.linspace(-size, size, 2 * size + 1)
-    y_range = np.linspace(-size, size, 2 * size + 1)
+    x_range = np.linspace(-(x_size - 1)/2, (x_size - 1)/2, x_size)
+    y_range = np.linspace(-(y_size - 1)/2, (y_size - 1)/2, y_size)
     x, y = np.meshgrid(x_range, y_range)
 
     # Rotação das coordenadas para orientar o filtro
@@ -20,8 +21,8 @@ def gabor_filter(size, theta, f, sigma, gamma, psi):
     
     return gauss * oscillation
 
-# Gera o filtro
-kernel = gabor_filter(6, -3 * np.pi / 5 , 0.14, 4.4, 0.1, 0)
+# Gera o filtro (x_size, y_size, theta, f, sigma, gamma, psi)
+kernel = gabor_filter(33, 33, -np.pi , 0.18, 5.5, 0.35, 0)
 
 # Carrega a imagem sem ruído
 img = cv2.imread('ROI.tif', cv2.IMREAD_GRAYSCALE)
@@ -32,7 +33,6 @@ filtered_img = cv2.filter2D(img, -1, kernel)
 # Salvar resultado
 cv2.imwrite("filteredImage.tif", filtered_img)
 
-# plt.figure(figsize=(6, 6))
-# plt.imshow(kernel, cmap='gray')
-# plt.axis('off')
-# plt.show()
+kernel_norm = cv2.normalize(kernel, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+
+cv2.imwrite('GaborKernel.png', kernel_norm)
