77097f89038f68243b50043258280e57734897b2
JoaoLuis06 <jlrdvignoli@gmail.com>
2026-07-06 18:38:51 -0300

    Ultimas alterações do projeto

--- GaborFilterBank.py
+++ GaborFilterBank.py
@@ -5,6 +5,9 @@ import cv2
 import numpy as np
 from scipy import signal
 
+# Vetor de escalas
+sigma = np.array([2, 5, 10, 20])
+
 # Vetor de ângulos
 theta = np.array([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 
                   90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180])
@@ -12,55 +15,87 @@ theta = np.array([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 
 # Matriz para guardar as imagens de saída
 matriz_imagens_filtradas = []
 matriz_imagens_threshold = []
+matriz_kernel_escala = []
 
 # Aplicação do banco de filtros de Gabor e Threshold
 for i in range(len(Anisotropic_Diffusion.semRuido)):
-    linha_filtrada = []
-    linha_filtrada_normalizada = []
+    
+    escala_filtrada = []
 
-    for j in theta:
+    for s in sigma:
         
-        # Normalização dos valores dos pixels da imagem para o range de [0, 1]
-        imagem_normalizada = Anisotropic_Diffusion.semRuido[i] / 255
+        angulos_filtrados = []
+        angulos_filtrados_normalizados = []
+        kernel_angulo = []
         
-        # Criação do Kernel do Filtro
-        kernel = Gabor_Filter.gabor_2d(33, j, 0.12, 5, 0.6, 0)
+        for j in theta:
+
+            # Normalização dos valores dos pixels da imagem para o range de [0, 1]
+            imagem_normalizada = Anisotropic_Diffusion.semRuido[i] / 255
+
+            # Criação do Kernel do Filtro
+            kernel = Gabor_Filter.gabor_2d(33, j, 0.12, s, 0.6, 0)
+
+            # Convolução da imagem com o Kernel
+            filtered_img = signal.convolve2d(imagem_normalizada, kernel, mode='same')
+
+            # Clip para remover os valores negativos
+            filtered_clip = np.clip(filtered_img, 0, None)
+            angulos_filtrados.append(filtered_clip)
+
+            # Salvamento das imagens dos kernels de Gabor
+            if i == 0:
+                kernel_norm = cv2.normalize(kernel, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+                kernel_angulo.append(kernel_norm)
+                
+                os.makedirs(f'ImagensSaida/kernel/Escala{s}', exist_ok=True)
+                caminho_kernel = os.path.join(f'ImagensSaida/Kernel/Escala{s}', f'Kernel_Angulo{j}.png')
+                cv2.imwrite(caminho_kernel, kernel_norm)
         
-        # Convolução da imagem com o Kernel
-        filtered_img = signal.convolve2d(imagem_normalizada, kernel, mode='same')
+        maximo_global_da_escala = np.max(angulos_filtrados)
+            
+        for img_atual, imagem in enumerate(angulos_filtrados):
+            
+            # Normalização para a escala de [0, 255] para visualização da imagem
+            imagem_filtrada_normalizada = ((imagem / maximo_global_da_escala) * 255).astype(np.uint8)
+            angulos_filtrados_normalizados.append(imagem_filtrada_normalizada)
+            
+            os.makedirs(f'ImagensSaida/Gabor/Imagem{i+1}/Escala{s}', exist_ok=True)
+            caminho_gabor = os.path.join(f'ImagensSaida/Gabor/Imagem{i+1}/Escala{s}', f'Gabor_Angulo{theta[img_atual]}.tif')
+            cv2.imwrite(caminho_gabor, imagem_filtrada_normalizada)
+    
+        escala_filtrada.append(angulos_filtrados_normalizados)
+    
+    matriz_imagens_filtradas.append(escala_filtrada)
+    matriz_kernel_escala.append(kernel_angulo)
 
-        # Clip para remover os valores negativos
-        filtered_clip = np.clip(filtered_img, 0, None)
-        linha_filtrada.append(filtered_clip)
-
-        # Salvamento das imagens dos kernels de Gabor
-        if i == 0:
-            kernel_norm = cv2.normalize(kernel, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-            caminho_kernel = os.path.join('ImagensSaida/Kernel', f'Kernel_Angulo{j}.png')
-            cv2.imwrite(caminho_kernel, kernel_norm)
-        
-    maximo_global = np.max(linha_filtrada)
-
-    linha_threshold = []
-
+    threshold_escala = []
+    
     # Aplicando o Threshold
-    for pos_atual, imagem in enumerate(linha_filtrada):
+    for s in range(len(sigma)):
         
-        # Normalização para a escala de [0, 255] para visualização da imagem
-        imagem_filtrada_normalizada = ((imagem / maximo_global) * 255).astype(np.uint8)
-        linha_filtrada_normalizada.append(imagem_filtrada_normalizada)
+        threshold_angulo = []
         
-        # Threshold binário
-        _, thresh = cv2.threshold(imagem_filtrada_normalizada, 0.3 * 255, 255, cv2.THRESH_BINARY)
+        for a in range(len(theta)):
+            
+            imagem = escala_filtrada[s][a]
+            
+            # Threshold binário
+            _, thresh = cv2.threshold(imagem, 0.3 * 255, 255, cv2.THRESH_BINARY)
 
-        # Conversão para inteiro
-        thresh = thresh.astype(np.uint8)
+            # Conversão para inteiro
+            thresh = thresh.astype(np.uint8)
 
-        linha_threshold.append(thresh)
+            threshold_angulo.append(thresh)
 
-        # Salvamento da imagem
-        angulo_atual = theta[pos_atual]
-        caminho_thresh = os.path.join('ImagensSaida/Threshold', f'Imagem{i+1}_Threshold_{angulo_atual}.tif')
-        cv2.imwrite(caminho_thresh, thresh)
-
-    matriz_imagens_threshold.append(linha_threshold)
+            # Salvamento da imagem
+            angulo_atual = theta[a]
+            escala_atual = sigma[s]
+            
+            os.makedirs(f'ImagensSaida/Threshold/Imagem{i+1}/Escala{escala_atual}', exist_ok=True)
+            caminho_thresh = os.path.join(f'ImagensSaida/Threshold/Imagem{i+1}/Escala{escala_atual}', f'Threshold_Angulo{angulo_atual}.tif')
+            cv2.imwrite(caminho_thresh, thresh)
+            
+        threshold_escala.append(threshold_angulo)
+    
+    matriz_imagens_threshold.append(threshold_escala)
--- RoseDiagram.py
+++ RoseDiagram.py
@@ -1,55 +1,73 @@
 import GaborFilterBank
+import Anisotropic_Diffusion
 import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 matriz__pixels = []
-strings_theta = [str(i) for i in GaborFilterBank.theta]
 
-
-for linha in GaborFilterBank.matriz_imagens_threshold:
-    linha_contabilizadora = []
+for i in range(len(Anisotropic_Diffusion.semRuido)):
     
-    for imagem in linha:
-        quantidade_pixels = np.sum(imagem == 255)
-        linha_contabilizadora.append(quantidade_pixels)
+    pixels_escala = []
+    
+    for s in range(len(GaborFilterBank.sigma)):
         
-    matriz__pixels.append(linha_contabilizadora)
-
+        pixels_angulo = []
+        
+        for t in range(len(GaborFilterBank.theta)):
+            
+            imagem = GaborFilterBank.matriz_imagens_threshold[i][s][t]
+            
+            quantidade_pixels = np.sum(imagem == 255)
+            pixels_angulo.append(quantidade_pixels)
+            
+        pixels_escala.append(pixels_angulo)
+        
+    matriz__pixels.append(pixels_escala)
+            
 # Diagrama de Rosas
-for img_atual, dados in enumerate(matriz__pixels): 
-    max_val = [max(dados) * 1.5] * len(GaborFilterBank.theta)
-
-    df = pd.DataFrame({
-        'categories': ['0', '', '', '15', '', '', '30', '', '', '45', '', '', '60', '', '', '75', '', '', '90', '', '', '105', '', '', '120', '', '', '135', '', '', '150', '', '', '165', '', '', '180'],
-        'scores': dados,
-        'max_values': max_val,
-    })
-    df['pct'] = df['scores'] / df['max_values']
-
-    N = len(df)
-    theta = np.linspace(0, np.pi, N, endpoint=False)
-    width = np.pi / N 
-
-    fig, ax = plt.subplots(figsize=(8, 4), subplot_kw={'projection': 'polar'})
-
-    bars = ax.bar(
-        theta + width/2, 
-        df['pct'], 
-        width=width, 
-        edgecolor='black', 
-        color='skyblue', 
-        alpha=0.7
-    )
-
-    ax.set_thetamin(0)
-    ax.set_thetamax(180)
-    ax.set_xticks(theta + width/2)
-    ax.set_xticklabels(df['categories'])
-    ax.set_yticklabels([])
-
-    plt.tight_layout()
-
-    caminho_diagrama = os.path.join('ImagensSaida/DiagramaDeRosas', f'Diagrama_Rosas_Imagem{img_atual + 1}.png')
-    plt.savefig(caminho_diagrama, dpi=300, bbox_inches='tight')
+for img_atual, imagens in enumerate(matriz__pixels):
+    
+    for esc_atual, dados_escalas in enumerate(imagens):
+        
+        max_val = [max(dados_escalas) * 1.5] * len(GaborFilterBank.theta)
+    
+        df = pd.DataFrame({
+            'categories': ['0', '', '', '15', '', '', '30', '', '', '45', '', '', '60', '', '', '75', '', '', '90', '', '', '105', '', '', '120', '', '', '135', '', '', '150', '', '', '165', '', '', '180'],
+            'scores': dados_escalas,
+            'max_values': max_val,
+        })
+        df['pct'] = df['scores'] / df['max_values']
+    
+        N = len(df)
+        theta = np.linspace(0, np.pi, N, endpoint=False)
+        width = np.pi / N 
+    
+        fig, ax = plt.subplots(figsize=(8, 4), subplot_kw={'projection': 'polar'})
+    
+        bars = ax.bar(
+            theta + width/2, 
+            df['pct'], 
+            width=width, 
+            edgecolor='black', 
+            color='skyblue', 
+            alpha=0.7
+        )
+    
+        ax.set_thetamin(0)
+        ax.set_thetamax(180)
+        ax.set_xticks(theta + width/2)
+        ax.set_xticklabels(df['categories'])
+        ax.set_yticklabels([])
+    
+        escala_atual = GaborFilterBank.sigma[esc_atual]
+        ax.set_title(f'($\sigma$ = {escala_atual})', pad=20, fontsize=14, fontweight='bold')
+    
+        plt.tight_layout()
+    
+        os.makedirs(f'ImagensSaida/DiagramaDeRosas/Escala{GaborFilterBank.sigma[esc_atual]}', exist_ok=True)
+        caminho_diagrama = os.path.join(f'ImagensSaida/DiagramaDeRosas/Escala{GaborFilterBank.sigma[esc_atual]}', f'Diagrama_Rosas_Imagem{img_atual + 1}.png')
+        
+        plt.savefig(caminho_diagrama, dpi=300, bbox_inches='tight')
+        plt.close(fig)
